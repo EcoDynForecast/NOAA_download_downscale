4d1b457cc02e0499be0e98d074333d79d092fd5d
Laura-Puckett <36464703+Laura-Puckett@users.noreply.github.com>
2018-09-06 08:46:00 -0400

    Add files via upload
    Still needs some work, but takes the first day of saved forecasts and combines into one dataframe

--- process_saved_forecasts.R
+++ process_saved_forecasts.R
@@ -1,20 +1,53 @@
 # to process saved NOAA dataninto one continous record
-# 
+# should change to get files directly from github
+# need to split by ensemble
+# make column to say which date the data is coming from
+# clean up and comment
 # 
 
-/Users/laurapuckett/Documents/Research/Fall 2018/SCCData-noaa-data/
-  
+
 st <- as.Date("2018-04-23")
 en <- as.Date(Sys.Date())
 date.list <- seq(st, en, by = "1 day")
-
+forecasts_combined = NULL
+forecast.files.list = list.files(path = "/Users/laurapuckett/Documents/Research/Fall 2018/SCCData-noaa-data/")
 for(i in 1:length(date.list)){
   temp.year = lubridate::year(date.list[i])
   temp.month = lubridate::month(date.list[i])
   if(temp.month<10){temp.month = paste("0",temp.month, sep = "")}
   temp.day = lubridate::day(date.list[i])
   if(temp.day<10){temp.day = paste("0",temp.day, sep = "")}
+  
   date.path = paste(temp.year,temp.month,temp.day, sep = "")
-  full.path = paste("/Users/laurapuckett/Documents/Research/Fall 2018/SCCData-noaa-data/",date.path,"gep_all_00z.csv", sep = "")
-  temp.data = read.csv(full.path)
+  
+  if(paste(date.path,"gep_all_00z.csv", sep = "")%in% forecast.files.list){
+    full.path = paste("/Users/laurapuckett/Documents/Research/Fall 2018/SCCData-noaa-data/",date.path,"gep_all_00z.csv", sep = "")
+    last.data = date.list[i]
+    }else if(paste(date.path,"gep_all_06z.csv", sep = "")%in% forecast.files.list){
+      full.path = paste("/Users/laurapuckett/Documents/Research/Fall 2018/SCCData-noaa-data/",date.path,"gep_all_06z.csv", sep = "")
+      last.data = date.list[i]
+      last.path = full.path
+    }else if(paste(date.path,"gep_all_12z.csv", sep = "")%in% forecast.files.list){
+      full.path = paste("/Users/laurapuckett/Documents/Research/Fall 2018/SCCData-noaa-data/",date.path,"gep_all_12z.csv", sep = "")
+      last.data = date.list[i]
+      last.path = full.path
+    }else if(paste(date.path,"gep_all_18z.csv", sep = "")%in% forecast.files.list){
+      full.path = paste("/Users/laurapuckett/Documents/Research/Fall 2018/SCCData-noaa-data/",date.path,"gep_all_18z.csv", sep = "")
+      last.data = date.list[i]
+      last.path = full.path
+    }else{
+      print(paste("You are missing a file for date: ", date.path, sep = ""))
+      diff.day = date.list[i] - last.data
+      full.path = last.path
+      }
+    
+  temp.data = read.csv(full.path) %>% 
+    mutate(forecast.date = lubridate::as_date(forecast.date)) %>%
+    filter(forecast.date == date.list[i])
+  forecasts_combined = rbind(forecasts_combined, temp.data)
+  temp.year = NULL
+  temp.month = NULL
+  temp.day = NULL
+  temp.data = NULL
+  full.path = NULL
 }
