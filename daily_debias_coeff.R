1464a3280b54baa1d97940c32755104fa2eb6309
Laura-Puckett <36464703+Laura-Puckett@users.noreply.github.com>
2018-12-02 15:06:31 -0500

    Add files via upload

--- daily_debias_coeff.R
+++ daily_debias_coeff.R
@@ -1,4 +1,4 @@
-daily_debias_coeff <- function(joined.data, nmembers){
+daily_debias_coeff <- function(joined.data){
   
   get_lm_coeff <- function(col.obs, col.for){
     model = lm(col.obs ~ col.for)
@@ -12,22 +12,25 @@ daily_debias_coeff <- function(joined.data, nmembers){
     res.sd = sd(residuals(model))
     return(res.sd)
   }
-  df = data_frame(temp = rep(NA,3), RH = rep(NA,3), ws = rep(NA,3), sw = rep(NA,3))
+  df = data_frame(temp = rep(NA,4), RH = rep(NA,4), ws = rep(NA,4), sw = rep(NA,4), lw = rep(NA,4))
   # slope
   df$temp[1] = get_lm_coeff(joined.data$temp.obs, joined.data$temp.for)[[1]]
   df$RH[1] = get_lm_coeff(joined.data$RH.obs, joined.data$RH.for)[[1]]
   df$ws[1] = get_lm_coeff(joined.data$ws.obs, joined.data$ws.for)[[1]]
   df$sw[1] = get_lm_coeff(joined.data$sw.obs, joined.data$sw.for)[[1]]
+  df$lw[1] = get_lm_coeff(joined.data$lw.obs, joined.data$lw.for)[[1]]
   # intercept
   df$temp[2] = get_lm_coeff(joined.data$temp.obs, joined.data$temp.for)[[2]]
   df$RH[2] = get_lm_coeff(joined.data$RH.obs, joined.data$RH.for)[[2]]
   df$ws[2] = get_lm_coeff(joined.data$ws.obs, joined.data$ws.for)[[2]]
   df$sw[2] = get_lm_coeff(joined.data$sw.obs, joined.data$sw.for)[[2]]
+  df$lw[2] = get_lm_coeff(joined.data$lw.obs, joined.data$lw.for)[[2]]
   # standard deviation of residuals
   df$temp[3] = lm.res.sd(joined.data$temp.obs, joined.data$temp.for)
   df$RH[3] = lm.res.sd(joined.data$RH.obs, joined.data$RH.for)
   df$ws[3] = lm.res.sd(joined.data$ws.obs, joined.data$ws.for)
   df$sw[3] = lm.res.sd(joined.data$sw.obs, joined.data$sw.for)
+  df$lw[3] = lm.res.sd(joined.data$lw.obs, joined.data$lw.for)
 
   return(df)
 }
--- daily_debias_from_coeff.R
+++ daily_debias_from_coeff.R
@@ -15,22 +15,10 @@ daily_debias_from_coeff <- function(daily_forecast, coeff.df){
     dplyr::mutate(temp.mod =  lin.mod(daily_forecast$temp, coeff.df$temp),
                   RH.mod =  lin.mod(daily_forecast$RH, coeff.df$RH),
                   ws.mod =  lin.mod(daily_forecast$ws, coeff.df$ws),
-                  sw.mod =  lin.mod(daily_forecast$sw, coeff.df$sw)) %>% 
-    select(date, NOAA.member, temp.mod, RH.mod, ws.mod, sw.mod)
-  
-  # 
-  debiased.with.noise <- debiased %>%
-    group_by(date, NOAA.member, temp.mod, RH.mod,  ws.mod, sw.mod) %>%
-    expand(dscale.member = 1:nmembers) %>%
-    ungroup() %>%
-    group_by(date, dscale.member, NOAA.member, temp.mod, RH.mod,  ws.mod, sw.mod) %>%
-    dplyr::mutate(temp.mod.noise = temp.mod + rnorm(mean = 0, sd = coeff.df$temp[3], n = 1),
-                  RH.mod.noise = RH.mod + rnorm(mean = 0, sd = coeff.df$RH[3], n = 1),
-                  ws.mod.noise = ws.mod + rnorm(mean = 0, sd = coeff.df$ws[3], n = 1),
-                  sw.mod.noise = sw.mod + rnorm(mean = 0, sd = coeff.df$sw[3], n = 1)) %>%
-    ungroup() %>%
-    select(date, dscale.member, NOAA.member, temp.mod, temp.mod.noise, RH.mod, RH.mod.noise, ws.mod, ws.mod.noise, sw.mod, sw.mod.noise)
-  
-  return(list(debiased, debiased.with.noise))
+                  sw.mod =  lin.mod(daily_forecast$sw, coeff.df$sw),
+                  lw.mod =  lin.mod(daily_forecast$lw, coeff.df$lw),
+                  dscale.member = 0) %>% 
+    select(date, NOAA.member, dscale.member, temp.mod, RH.mod, ws.mod, sw.mod, lw.mod)
+  return(debiased)
 }
 
